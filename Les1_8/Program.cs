c6d276ae1a67ee11d5ba6f0ca2794a6f7e1774fd
Yaroslav-Solovev <workbox72@bk.ru>
2022-08-09 19:23:43 +0500

    ДЗ урок 1 задача 8

---
+++ Les1_8/Program.cs
@@ -0,0 +1,19 @@
+﻿//Console.WriteLine("Программа, которая на вход принимает одно число (N), а на выходе показывает все четные числа в промежутке от 1 до N");
+//Console.WriteLine("Введите число ");
+Console.WriteLine("Введите положительное число ");
+
+string numberString = Console.ReadLine()!;
+
+int number = int.Parse(numberString);
+
+if (number <= 0)
+{
+    Console.WriteLine("Вы ввели отрицательное число (или ноль). Заново ведите число");
+}
+else
+{
+    for (int i = 2; i < number; i++)
+    if (i%2 == 0)
+    Console.WriteLine(i);  
+}
+
