8dc4cc57e51e84d997027cc81b4a898fcc2ed935
Yaroslav-Solovev <workbox72@bk.ru>
2022-08-14 00:48:04 +0500

    ДЗ урок 3 задача 23

---
+++ Les3_23/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N");
+Console.WriteLine("\nВведите число");
+
+string numberString = Console.ReadLine()!;
+
+int number = int.Parse(numberString);
+
+int index = 1;
+
+Console.WriteLine("\nТаблица кубов введенного числа выглядит следующим образом: ");
+
+if (number <= 0)
+    Console.WriteLine("\nError: Программа не выдаст таблицу кубов чисел от 1 до N. Введите положительное число от 1.");
+
+while (index <= number) 
+{
+    Console.WriteLine(index*index*index);
+    index += 1;
+}
