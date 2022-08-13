bbb211a122a9127e4bdf66ee69c83977fb63da68
Yaroslav-Solovev <workbox72@bk.ru>
2022-08-13 23:45:09 +0500

    ДЗ урок 2 задача 13

---
+++ Les2_13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine("\nПрограмма, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+
+Console.WriteLine("\nВведите число ");
+
+string? numberString = Console.ReadLine();
+
+var number = int.Parse(numberString!);
+
+if (number < 0)
+    number *= (-1);
+
+if (number > 99)
+{
+    Console.WriteLine("\nТретья цифра введенного числа: " + number.ToString()[2]);
+}
+else
+{
+    Console.WriteLine("\nТретьей цифры нет");
+}
