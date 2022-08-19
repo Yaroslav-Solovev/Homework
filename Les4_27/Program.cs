bca1bd7fb714b4c4ddeb5c52e2ac8f1fe15e5c47
Yaroslav-Solovev <workbox72@bk.ru>
2022-08-19 20:21:21 +0500

    ДЗ урок 4 задача 27

---
+++ Les4_27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Программа, которая принимает на вход число и выдает сумму цифр в числе.");
+Console.WriteLine("\nВведите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int number)  // сумма цифр в числе
+{
+    int count = Convert.ToString(number).Length;
+    int step = 0;
+    int result = 0;
+    for (int i = 0; i < count; i++)
+    {
+        step = number - number % 10;
+        result += number - step;
+        number /= 10;
+    }
+    return result;
+}
+
+int sumNumber = SumNumber(number);
+
+Console.WriteLine("Сумма цифр веденного числа: " + sumNumber);
