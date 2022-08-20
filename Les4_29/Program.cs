36b56c9840cb9bc97171fe1c0a9dd5a7c3cfe36a
Yaroslav-Solovev <workbox72@bk.ru>
2022-08-21 01:03:34 +0500

    ДЗ урок 4 задача 29

---
+++ Les4_29/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("\nПрограмма, которая задаёт массив из 8 элементов и выводит их на экран.");
+
+// Задаем массив из 8 элементов
+int[] createArray()
+{
+    var random = new Random();
+    var result = new int[8];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = random.Next(1, 20);
+    }
+    return result;
+}
+
+// Функция вывода массива
+void viewArr(int[] arr)
+{
+    string result = "[";
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+        {
+            result += arr[i] + ", ";
+        }
+        if (i == arr.Length - 1)
+        {
+            result += arr[i] + "]";
+        }
+    }
+    Console.WriteLine(result);
+}
+
+int[] array = createArray();
+viewArr(array);
