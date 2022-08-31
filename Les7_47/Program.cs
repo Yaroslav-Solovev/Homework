707f0c10c56f57f75ddd3b098f1d9d3fb27118f6
Yaroslav-Solovev <workbox72@bk.ru>
2022-08-31 19:47:31 +0500

    ДЗ урок 7 задача 47

---
+++ Les7_47/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// Функция вывода массива
+void PrintArray(double[,] array){
+    for (var i = 0; i < array.GetLength(0); i++){
+        for (var j = 0; j < array.GetLength(1); j++){
+            array[i,j] = Math.Round(array[i,j], 1, MidpointRounding.ToEven);
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Функция рандомного формирования массива вещественными числами
+double[,] CreateArrayWithRandomNumbers(int m, int n) {
+    double[,] result = new double[m,n];
+
+    var random = new Random();
+
+    for (var i = 0; i < result.GetLength(0); i++) {
+        for (var j = 0; j < result.GetLength(1); j++) {
+            result[i,j] = random.Next(-10,10) + random.NextDouble();
+        }
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число строк (m)");
+if(!int.TryParse(Console.ReadLine()!, out var m)) {
+    Console.WriteLine("Error");
+}
+
+Console.WriteLine("Введите число столбцов (n)");
+if(!int.TryParse(Console.ReadLine()!, out var n)) {
+    Console.WriteLine("Error");
+}
+
+PrintArray(CreateArrayWithRandomNumbers(m, n));
+
+
+
+
